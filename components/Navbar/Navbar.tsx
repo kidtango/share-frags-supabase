83ea5508c38cfae78a71c02afe13bf6ab0a53d44
Scott Tang <fq600@yahoo.com>
2022-04-06 13:21:40 -0500

    added login modal and fixed avatar/user menu glitch with hydration

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -10,8 +10,6 @@ import { AiOutlineHeart } from 'react-icons/ai'
 // import UserMenu from '../user-menu/UserMenu'
 import IconButton from '@components/buttons'
 import ShareFragLogo from './ShareFragLogo'
-import ToggleMenuButton from './ToggleMenuButton'
-import SearchBar from '@components/search-bar'
 import UserNav from './UserNav'
 
 interface INavbar {
@@ -40,38 +38,6 @@ const Navbar: React.FC<INavbar> = ({ isSidebarOpen, setIsSidebarOpen }) => {
   }
 
   return (
-    // <header className="bg-opacity-50 backdrop-filter backdrop-blur sticky top-0 z-30 h-[72px] w-full border-b shadow items-center flex">
-    //   <div className="container mx-auto">
-    //     <div className="flex items-center space-x-1">
-    //       {/* mobile navbar */}
-
-    //       {/* Navbar left */}
-
-    //       {/* Toggle Sidebar Button */}
-    //       <div className="mr-2">
-    //         <ShareFragLogo />
-    //       </div>
-    //       <SearchBar />
-    //       {/* Navbar Right */}
-    //       {session ? (
-    //         <div className="flex items-center space-x-2">
-    //           <span className="hidden sm:block"></span>
-    //           <div className="hidden space-x-1 md:flex"></div>
-    //           <div className="mt-1">
-    //             {/* <UserMenu session={session!} signout={signout} /> */}
-    //             <button className="btn btn-primary">Hello daisyUI</button>
-    //           </div>
-    //         </div>
-    //       ) : (
-    //         <div className="">
-    //           <LoginButton />
-    //         </div>
-    //       )}
-
-    //       {/* mobile navbar */}
-    //     </div>
-    //   </div>
-    // </header>
     <div className="bg-white navbar">
       <div className="flex-1">
         <a className="text-xl normal-case btn btn-ghost">shareFrags</a>
@@ -84,26 +50,7 @@ const Navbar: React.FC<INavbar> = ({ isSidebarOpen, setIsSidebarOpen }) => {
             className="rounded-full input input-bordered"
           />
         </div>
-        {/* notification */}
-        {/* <button className="btn btn-ghost btn-circle">
-          <div className="indicator">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="w-5 h-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-              />
-            </svg>
-            <span className="badge badge-xs badge-primary indicator-item"></span>
-          </div>
-        </button> */}
+
         <div className="flex-none">
           <div className="dropdown dropdown-end">
             <label tabIndex={0} className="btn btn-ghost btn-circle">
--- components/Navbar/UserNav/UserNav.tsx
+++ components/Navbar/UserNav/UserNav.tsx
@@ -1,32 +1,55 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Image from 'next/image'
+import { AiOutlineUser } from 'react-icons/ai'
+
+import Auth from '@components/Auth'
 import Login from './Login'
+import { User } from '@supabase/supabase-js'
 
 const UserNav = () => {
+  const { user } = Auth.useUser()
+  const [currentUser, setCurrentUser] = useState<User>()
+
+  useEffect(() => {
+    if (user) setCurrentUser(user)
+  }, [user])
+
   return (
     <div className="dropdown dropdown-end">
-      <Login />
-      {/* <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-        <div className="w-10 rounded-full">
-          <img src="https://api.lorem.space/image/face?hash=33791" />
-        </div>
-      </label>
-      <ul
-        tabIndex={0}
-        className="p-2 mt-3 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
-      >
-        <li>
-          <a className="justify-between">
-            Profile
-            <span className="badge">New</span>
-          </a>
-        </li>
-        <li>
-          <a>Settings</a>
-        </li>
-        <li>
-          <a>Logout</a>
-        </li>
-      </ul> */}
+      {currentUser ? (
+        <>
+          <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+            <div className="w-10 rounded-full">
+              <Image
+                src={currentUser?.user_metadata?.avatar_url}
+                alt="avatar"
+                layout="responsive"
+                height={10}
+                width={10}
+              />
+            </div>
+          </label>
+          <ul
+            tabIndex={0}
+            className="p-2 mt-3 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <a className="justify-between">
+                Profile
+                <span className="badge">New</span>
+              </a>
+            </li>
+            <li>
+              <a>Settings</a>
+            </li>
+            <li>
+              <a>Logout</a>
+            </li>
+          </ul>
+        </>
+      ) : (
+        <Login />
+      )}
     </div>
   )
 }
